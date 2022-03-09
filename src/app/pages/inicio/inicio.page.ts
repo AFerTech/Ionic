3d1349b10a9d4301fe230e6823848a14121427af
Alexis Fernando <alexis96fer@gmail.com>
2022-03-09 00:41:08 -0600

    se creó el check page y se le agregó ion-checkbox

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -38,6 +38,11 @@ export class InicioPage implements OnInit {
       icon: 'card-outline',
       name: 'Cards',
       redirectTo: '/card'
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checks',
+      redirectTo: '/check'
     }
       ];
 
