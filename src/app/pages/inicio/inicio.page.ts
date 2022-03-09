46d143ef8ca338805c967eb62db8e06a1b826445
Alexis Fernando <alexis96fer@gmail.com>
2022-03-09 15:54:32 -0600

    page fab, ion-fab creados y cambio de variables de color en css

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -48,6 +48,11 @@ export class InicioPage implements OnInit {
       icon: 'calendar-outline',
       name: 'DateTime',
       redirectTo: '/date-time'
+    },
+    {
+      icon: 'car-outline',
+      name: 'Fab',
+      redirectTo: '/fab'
     }
       ];
 
